f37836b9f7bc96adece8b02c2cf11583b99aa14f
felipeyamana <felipedias0810@gmail.com>
2025-09-10 21:44:47 -0300

    removing unused folders and files

--- src/AngularApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AngularApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
