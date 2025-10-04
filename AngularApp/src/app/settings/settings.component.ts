a3094d43aaca642dc9328a109fd501961457d302
felipeyamana <felipedias0810@gmail.com>
2025-10-04 20:24:11 -0300

    adding navbar to the settings components

--- AngularApp/src/app/settings/settings.component.ts
+++ AngularApp/src/app/settings/settings.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeUiComponent } from './theme-ui/theme-ui.component';
+import { Navbar } from '../components/navbar/navbar';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule, ThemeUiComponent],
+  imports: [CommonModule, Navbar, ThemeUiComponent],
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
