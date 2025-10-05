33e1ddbe0567be35294387d4f400c4db33c73f85
felipeyamana <felipedias0810@gmail.com>
2025-10-05 16:25:04 -0300

    including the actual entity files that were ignored before

---
+++ src/Domain/Entities/Logs/Log.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities.Logs
+{
+    public class Log
+    {
+        public long Id { get; set; }
+
+        public int LogTypeId { get; set; }
+        public LogType LogType { get; set; } = null!;
+
+        public string? UserId { get; set; }
+        public ApplicationUser? User { get; set; }
+
+        [MaxLength(150)]
+        public string Action { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [MaxLength(45)]
+        public string? IpAddress { get; set; }
+
+        public DateTime EventDate { get; set; } = DateTime.UtcNow;
+    }
+}
